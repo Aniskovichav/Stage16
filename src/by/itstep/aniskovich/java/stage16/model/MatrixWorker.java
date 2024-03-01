ab312acd96733e7a7e447d572eddef8a792095b7
Timmi1015 <aniskovichav@gmail.com>
2024-03-01 23:34:44 +0300

    add MatrixWorker.sumBetweenFirstAndSecondPositiveElement

--- src/by/itstep/aniskovich/java/stage16/model/MatrixWorker.java
+++ src/by/itstep/aniskovich/java/stage16/model/MatrixWorker.java
@@ -2,6 +2,10 @@ package by.itstep.aniskovich.java.stage16.model;
 
 public class MatrixWorker {
     public static int calcBoundarySum(int[][] matrix) { // best - middle - worst: O(N+M)
+        if (matrix == null || matrix.length == 0
+                || matrix.length != matrix[0].length) {
+            return -1;
+        }
         int sum = 0;
         int rows = matrix.length;
         int columns = matrix[0].length;
@@ -18,6 +22,7 @@ public class MatrixWorker {
 
         return sum;
     }
+
     public static boolean isMagicMatrix(int[][] matrix) {
         if (matrix == null || matrix.length == 0
                 || matrix.length != matrix[0].length) {
@@ -27,11 +32,11 @@ public class MatrixWorker {
         int n = matrix.length;
         int magicSum = n * (n * n + 1) / 2;
 
-        for (int i = 0; i < n;i++) {    // best - middle - worst: O(N^2)
+        for (int i = 0; i < n; i++) {    // best - middle - worst: O(N^2)
             if (magicSum != sumRow(matrix, i)
                     || magicSum != sumColumn(matrix, i)
                     || magicSum != sumDiagonal(matrix)
-                    || magicSum != sumSecondaryDiagonal(matrix)){
+                    || magicSum != sumSecondaryDiagonal(matrix)) {
                 return false;
             }
         }
@@ -78,5 +83,43 @@ public class MatrixWorker {
         return sum;
     }
 
+    public static int sumBetweenFirstAndSecondPositiveElement(int[][] matrix) {   // best - middle - worst: O(N*M*K)
+        if (matrix == null || matrix.length == 0) { // O(1)
+            return 0;
+        }
 
+        int sum = 0;
+
+        for (int i = 0; i < matrix.length; i++) { // O(N*K)
+            int firstIndex = -1;
+            int secondIndex = -1;
+
+            for (int j = 0; j < matrix[i].length; j++) { // O(M)
+                if (matrix[i][j] > 0 && firstIndex == -1) {
+                    firstIndex = j;
+                } else if (matrix[i][j] > 0 && secondIndex == -1) {
+                    secondIndex = j;
+                    break;
+                }
+            }
+            sum += sumRowBetweenFirstAndSecondDesiredEl(matrix, i, firstIndex,
+                    secondIndex);
+        }
+
+        return sum;
+    }
+
+    private static int sumRowBetweenFirstAndSecondDesiredEl(int[][] matrix,
+                                                 int i, int first, int second) { //Best O(1) Middle O(K) Worst O(N)
+        if (first == -1 || second == -1) {
+          return 0;
+        }
+
+        int sum = 0;
+
+        for (int j = first + 1; j < second; j++) {
+            sum += matrix[i][j];
+        }
+        return sum;
+    }
 }
