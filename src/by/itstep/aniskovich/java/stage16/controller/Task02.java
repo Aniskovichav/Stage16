7a3bc66eb200d73e26a2a2b09089acf20b405978
Timmi1015 <aniskovichav@gmail.com>
2024-03-03 23:17:26 +0300

    add Task02, Task03

--- src/by/itstep/aniskovich/java/stage16/controller/Task02.java
+++ src/by/itstep/aniskovich/java/stage16/controller/Task02.java
@@ -1,7 +1,31 @@
 package by.itstep.aniskovich.java.stage16.controller;
 
+import by.itstep.aniskovich.java.stage16.model.MatrixWorker;
+import by.itstep.aniskovich.java.stage16.util.MatrixInit;
+import by.itstep.aniskovich.java.stage16.util.MatrixUI;
+import by.itstep.aniskovich.java.stage16.view.ConsolePrinter;
+
+import static by.itstep.aniskovich.java.stage16.util.MatrixInit.initializeMatrix;
+import static by.itstep.aniskovich.java.stage16.util.MatrixInit.randomSize;
+import static by.itstep.aniskovich.java.stage16.view.ConsolePrinter.printMatrix;
+
 public class Task02 {
     public static void main(String[] args) {
+        int[][] matrix = initializeMatrix(randomSize());
 
+        MatrixInit.randomInitElement(matrix);
+
+
+//        int[][] matrix1 = {
+//                {2, 9, 4, 5},
+//                {3, 5, -3, 2},
+//                {6, 0, -6, 8},
+//        };
+
+        ConsolePrinter.print(printMatrix(matrix));
+
+        ConsolePrinter.print(MatrixUI.defineMonotonicColumnUI
+                (MatrixWorker.defineMonotonicColumns(matrix)));
     }
 }
+
--- src/by/itstep/aniskovich/java/stage16/controller/Task03.java
+++ src/by/itstep/aniskovich/java/stage16/controller/Task03.java
@@ -1,4 +1,31 @@
 package by.itstep.aniskovich.java.stage16.controller;
 
+import by.itstep.aniskovich.java.stage16.model.MatrixWorker;
+import by.itstep.aniskovich.java.stage16.util.MatrixInit;
+import by.itstep.aniskovich.java.stage16.util.MatrixUI;
+import by.itstep.aniskovich.java.stage16.view.ConsolePrinter;
+
+import static by.itstep.aniskovich.java.stage16.util.MatrixInit.initializeMatrix;
+import static by.itstep.aniskovich.java.stage16.util.MatrixInit.randomSize;
+import static by.itstep.aniskovich.java.stage16.view.ConsolePrinter.printMatrix;
+
 public class Task03 {
+    public static void main(String[] args) {
+        int[][] matrix = initializeMatrix(randomSize());
+
+        MatrixInit.randomInitElement(matrix);
+
+
+        int[][] matrix1 = {
+                {2, 3, 4, 5},
+                {3, 5, -3, 2},
+                {6, 7, 8, 9},
+        };
+
+//        ConsolePrinter.print(printMatrix(matrix));
+
+        ConsolePrinter.print(MatrixUI.findMaxElementAscendingRowsUI
+                (MatrixWorker.findMaxElementAscendingRows(matrix1)));
+    }
 }
+
