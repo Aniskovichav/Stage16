1305b619754740e9741e55fd45242ecab216d6d4
Timmi1015 <aniskovichav@gmail.com>
2024-03-04 14:03:35 +0300

    add Task05

--- src/by/itstep/aniskovich/java/stage16/controller/Task05.java
+++ src/by/itstep/aniskovich/java/stage16/controller/Task05.java
@@ -1,4 +1,31 @@
 package by.itstep.aniskovich.java.stage16.controller;
 
+import by.itstep.aniskovich.java.stage16.model.MatrixWorker;
+import by.itstep.aniskovich.java.stage16.util.MatrixInit;
+import by.itstep.aniskovich.java.stage16.util.MatrixUI;
+import by.itstep.aniskovich.java.stage16.view.ConsolePrinter;
+
+import static by.itstep.aniskovich.java.stage16.util.MatrixInit.initializeMatrix;
+import static by.itstep.aniskovich.java.stage16.util.MatrixInit.randomSize;
+
 public class Task05 {
+    public static void main(String[] args) {
+//        int[][] matrix = initializeMatrix(randomSize());
+//
+//        MatrixInit.randomInitElement(matrix);
+
+
+        int[][] matrix1 = {
+                {1, 2, 3, 6, 7, 8, 9},
+                {4, 4, 4, 5},
+                {0, 1, 2, 3, 4, 7, 8, 9},
+                {8, 8, 8},
+                {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+        };
+
+//        ConsolePrinter.print(printMatrix(matrix));
+
+        ConsolePrinter.print(MatrixUI.defineRowWithMaxAscendingCountUI
+            ((MatrixWorker.defineRowWithMaxAscendingCount(matrix1))));
+    }
 }
